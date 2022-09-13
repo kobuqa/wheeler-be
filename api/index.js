a93c808c2c4f79ad0d9a51cb4885613ff331d4a2
Alexander Matveichikov <alexander.matveichikov@mbicycle.com>
2022-09-13 13:15:44 +0300

    feat: make vercel works2

--- api/index.js
+++ api/index.js
@@ -9,11 +9,8 @@ const app = express();
 // app.use(bodyParser)
 
 
-app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+app.get('/', (req, res) => {
+res.send('hello world')
   });
 
 
